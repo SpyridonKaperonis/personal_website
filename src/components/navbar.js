d8b64f810b4930a24df3259d8619f1a227f0b208
SpyridonKaperonis <spyridonkaperonis@gmail.com>
2022-01-06 09:44:22 -0500

    first commit

---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class navbar extends React.Component {
+
+    render() {
+        return (
+            
+            <div>
+
+            </div>
+            <h1>Hello, this is Spyridon</h1>
+        );
+    }
+    
+}
+
+export default navbar;
